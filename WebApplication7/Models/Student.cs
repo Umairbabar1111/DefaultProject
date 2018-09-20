146a9073af23544309f08a34f69b52dcffe20f2c
umairbabar1996 <umairbabar1996@gmail.com>
2018-09-20 16:49:48 +0500

    C2 EntityFramework

---
+++ WebApplication7/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication7.Models
+{
+    public partial class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Rollno { get; set; }
+        public string Image { get; set; }
+        public string Cv { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ WebApplication7/Models/TasksContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApplication7.Models
+{
+    public partial class TasksContext : DbContext
+    {
+        public TasksContext()
+        {
+        }
+
+        public TasksContext(DbContextOptions<TasksContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Student { get; set; }
+        public virtual DbSet<Teacher> Teacher { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-7I0ATC2;Database=Tasks;Trusted_Connection=True;User ID=sa;Password=88669966@@;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.ConfirmPassword).HasMaxLength(50);
+
+                entity.Property(e => e.Cv)
+                    .HasColumnName("CV")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.Image).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+
+                entity.Property(e => e.Password).HasMaxLength(50);
+
+                entity.Property(e => e.Rollno).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Teacher>(entity =>
+            {
+                entity.Property(e => e.ConfirmPassword).HasMaxLength(50);
+
+                entity.Property(e => e.Cv)
+                    .HasColumnName("CV")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.Experiance).HasMaxLength(50);
+
+                entity.Property(e => e.Image).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+
+                entity.Property(e => e.Password).HasMaxLength(50);
+            });
+        }
+    }
+}
