552fe533164bf8b82ee887b0b72387d54cd57e15
umairbabar1996 <umairbabar1996@gmail.com>
2018-09-23 01:37:52 +0500

    c9

--- WebApplication7/Controllers/StudentController.cs
+++ WebApplication7/Controllers/StudentController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication7.Models;
 
@@ -10,8 +14,10 @@ namespace WebApplication7.Controllers
     public class StudentController : Controller
     {
         TasksContext mydb = null;
-        public StudentController(TasksContext abc)
+        IHostingEnvironment _env = null;
+        public StudentController(TasksContext abc, IHostingEnvironment xyz)
         {
+            this._env = xyz;
             mydb = abc;
         }
 
@@ -23,17 +29,69 @@ namespace WebApplication7.Controllers
         }
 
         [HttpPost]
-        public IActionResult Student(Student S)
+        public IActionResult Student(Student S, IFormFile Image, IFormFile Cv)
         {
-            mydb.Student.Add(S);
-            mydb.SaveChanges();
-            return View();
-        }
+            bool x = mydb.Student.Where(a => a.Email == S.Email).Any();
+          if(x)  {
+                ViewBag.Message = "Email Already Exists";
+                return View();
+                
+            }
+            string path = _env.WebRootPath + "/Data/StudentsImages/";
+            var E = Path.GetExtension(Image.FileName);
+            var Name = DateTime.Now.ToString("yymmddhhmmss") + E;
+            var f = new FileStream(path + E, FileMode.Create);
+            Image.CopyTo(f);
+            f.Close();
+            S.Image = "/Data/StudentsImages/" + Name;
+
+            string C = _env.WebRootPath + "/Data/StudentsDocuments/";
+            var O = Path.GetExtension(Cv.FileName);
+            var N = DateTime.Now.ToString("yymmhhddmmss") + O;
+            var fs = new FileStream(C + O, FileMode.Create);
+            Cv.CopyTo(fs);
+            fs.Close();
+            S.Cv = "/Data/StudentsDocuments/" + N;
+            try
+            {
+                var msg = new MailMessage();
+                msg.From = new MailAddress("Registration Mail");
+                msg.To.Add(new MailAddress(S.Email));
+                msg.CC.Add(new MailAddress("farhan@gmail.com"));
+                msg.Subject = "Registration Mail";
+                msg.Body = "Dear" + @S.Name + "Thanks" + "For" + "Registration";
+                msg.IsBodyHtml = true;
+                if (!string.IsNullOrEmpty(@S.Image))
+                    msg.Attachments.Add(new Attachment(path + Name));
+                if (!string.IsNullOrEmpty(@S.Cv))
+                    msg.Attachments.Add(new Attachment(C + N));
+
+
+                SmtpClient sc = new SmtpClient();
+                sc.Credentials = new System.Net.NetworkCredential();
+                sc.Host = "smtp.gmail.com";
+                sc.Port = 587;
+                sc.EnableSsl = true;
+                sc.Send(msg);
+            }
+            catch (Exception w)
+            { }
+            
+                mydb.Student.Add(S);
+                mydb.SaveChanges();
+                return View();
+            }
+        [HttpGet]
         public IActionResult AllStudents()
         {
-            IList<Student> S = new List<Student>();
-            S = mydb.Student.ToList<Student>();
+            IList<Student> S = mydb.Student.ToList<Student>();
             return View(S);
+        }[HttpPost]
+        public IActionResult AllStudents(string SearchByName, string SearchByEmail)
+        {
+            IList<Student> p = mydb.Student.Where(a => a.Name.Contains(SearchByName) || a.Email.Contains(SearchByEmail)).ToList<Student>();
+            return View(p);
+
         }
         [HttpGet]
         public IActionResult EditStudent(int Id)
@@ -60,5 +118,17 @@ namespace WebApplication7.Controllers
             mydb.SaveChanges();
             return RedirectToAction("AllStudents");
         }
+
+        public FileResult DownloadCv(string path)
+        {
+            
+                if (path == null)
+                {
+                    ViewBag.Message = "Invalid Path";
+                    return null;
+                }
+                return File(path, new MimeSharp.Mime().Lookup(path), DateTime.Now.ToString("yymmddhhmmss") + System.IO.Path.GetExtension(path));
+            }        
+        
     }
 }
--- WebApplication7/Controllers/TeacherController.cs
+++ WebApplication7/Controllers/TeacherController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication7.Models;
 
@@ -10,8 +14,10 @@ namespace WebApplication7.Controllers
     public class TeacherController : Controller
     {
         TasksContext mydb = null;
-        public TeacherController(TasksContext abc)
+        IHostingEnvironment _env = null;
+        public TeacherController(TasksContext abc,IHostingEnvironment xyz)
         {
+            this._env = xyz;
             mydb = abc;
         }
 
@@ -21,19 +27,64 @@ namespace WebApplication7.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Teacher(Teacher T)
+        public IActionResult Teacher(Teacher T,IFormFile Image,IFormFile Cv)
         {
+            bool Email = mydb.Teacher.Where(p => p.Email == T.Email).Any();
+            if (Email){
+                ViewBag.Message = "Email Already Exists";
+                return View();
+
+            }
+            string path = _env.WebRootPath + "/Data/TeachersImages/";
+          string FileExt=  Path.GetExtension(Image.FileName);
+            string Name = DateTime.Now.ToString("yymmddhhmmss") + FileExt;
+            FileStream fs = new FileStream(path + FileExt, FileMode.Create);
+            Image.CopyTo(fs);
+            fs.Close();
+            T.Image = "/Data/TeachersImages/" + Name;
+
+            string CVPath = _env.WebRootPath+"/Data/TeachersDocuments/";
+            string Ext = Path.GetExtension(Cv.FileName);
+            string NameCV = DateTime.Now.ToString("yymmddhhmmss")+Ext;
+            FileStream cs = new FileStream(CVPath + Ext, FileMode.Create);
+            Cv.CopyTo(cs);
+            cs.Close();
+            T.Cv = "/Data/TeacherDocuments/" + NameCV;
+
             mydb.Teacher.Add(T);
             mydb.SaveChanges();
+
+
+
+
+            var msg = new MailMessage();
+            msg.From = new MailAddress("umairbabr1996@gmail.com", "Registration Mail");
+            msg.To.Add(new MailAddress(T.Email));
+            msg.CC.Add(new MailAddress("Farhan1996@gmail.com"));
+            msg.Subject = "ragistration Mail";
+            msg.Body = "Dear" + T.Name + "Thanks For Registration";
+            msg.IsBodyHtml = true;
+            if(!string.IsNullOrEmpty(@T.Image))
+            msg.Attachments.Add(new Attachment(path + T.Image));
+            SmtpClient sc = new SmtpClient();
+            sc.Credentials =  new System.Net.NetworkCredential("umairbabar1996@gmail.com","88669966@@");
+            sc.Host = "smpt.gmail.com";
+            sc.Port = 587;
+            sc.EnableSsl = true;
+            sc.Send(msg);
             return View();
         }
 
-
+        [HttpGet]
         public IActionResult AllTeachers()
         {
-            IList<Teacher> T = new List<Teacher>();
-            T = mydb.Teacher.ToList<Teacher>();
+            IList<Teacher> T = mydb.Teacher.ToList<Teacher>();
             return View(T);
+        }[HttpPost]
+        public IActionResult AllTeachers(string SearchByName,string SearchByEmail)
+        {
+            IList<Teacher> p = mydb.Teacher.Where(a => a.Name.Contains(SearchByName) || a.Email.Contains(SearchByEmail)).ToList<Teacher>();
+            return View(p);
         }
 
         [HttpGet]
@@ -61,5 +112,16 @@ namespace WebApplication7.Controllers
             return RedirectToAction("AllTeachers");
 
         }
+        public FileResult DownloadCv(string path)
+        {if (path == null) {
+                ViewBag.Message = "Invalid Path";
+                return null;           }
+            return File(path,new MimeSharp.Mime().Lookup(path),DateTime.Now.ToString("yymmddhhmmss")+System.IO.Path.GetExtension(path));
+        }
+        public IActionResult AllTeacher()
+        {
+            return View(mydb.Teacher.ToList<Teacher>());
+        }
+
     }
 }
